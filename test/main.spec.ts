ea43788d31045174d89c8cdd09c159fa0ce19d93
Leto <leto4100@gmail.com>
2022-04-22 14:57:20 +0100

    proceso

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,10 +1,10 @@
-import { MarkdownTransform } from '../src/MarkdownTransform';
+import {MarkdownTransform} from '../src/MarkdownTransform';
 
 
 describe('Markdown transformation', () => {
   const transform = new MarkdownTransform();
   it('Transform markdown link to anchor', () => {
-    expect(transform.linkToFootnote('[visible text link](url)')).toBe('visible text link [^anchor1]\n\n[^anchor1]: url')
-    expect(transform.linkToFootnote('Hola que tal [visible text link](url) jaja adios')).toBe('Hola que tal visible text link [^anchor1] jaja adios\n\n[^anchor1]: url')
+    expect(transform.linkToFootnote('[visible text link](url)')).toBe('visible text link [^anchor1]\n\n[^anchor1]: url');
+    expect(transform.linkToFootnote('Hola que tal [visible text link](url) jaja adios')).toBe('Hola que tal visible text link [^anchor1] jaja adios\n\n[^anchor1]: url');
   });
 });
